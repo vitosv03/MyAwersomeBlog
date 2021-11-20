31df930058a1199e73544267123aaae66004b56a
vitosv03 <vitosv03@gmail.com>
2021-11-20 17:31:42 +0200

    connect PostGreSQl to our site

--- myawersomeblog-project/myawersomeblog/settings.py
+++ myawersomeblog-project/myawersomeblog/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = 'myawersomeblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # old basic database
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myawersomeblogdb',
+        'USER': 'postgres',
+        'PASSWORD': 'PyPass-99',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
