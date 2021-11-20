62a45ba37b46c9d3462922d6487ee2d2249a7337
vitosv03 <vitosv03@gmail.com>
2021-11-20 21:40:23 +0200

    1. set up on home.html loading images and description for it from model Events 2. edit views.py  {'events': events}

--- myawersomeblog-project/events/views.py
+++ myawersomeblog-project/events/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Event
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'events/home.html')
+    events = Event.objects
+    return render(request,
+                  'events/home.html',
+                  {
+                      'events': events
+                  }
+                  )
