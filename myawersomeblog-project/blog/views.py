b8fdae736f0c049750ab5932626addd11421aa85
vitosv03 <vitosv03@gmail.com>
2021-11-20 23:21:04 +0200

    add blog functionality 1. create blog\urls.py 2. myawersomeblog\urls.py add route (include blog.urls) 3.add into blog\urls.py routes via blog\views.py to blog\templates\blog\blog.html

--- myawersomeblog-project/blog/views.py
+++ myawersomeblog-project/blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def showblog(request):
+    return render(request, 'blog/blog.html')
--- myawersomeblog-project/myawersomeblog/urls.py
+++ myawersomeblog-project/myawersomeblog/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
 import events.views
@@ -22,4 +23,5 @@ import events.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', events.views.home, name='home'),
+    path('posts/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
