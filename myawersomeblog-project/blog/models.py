714499a0ad67046bc970017e85502823655c5c8e
vitosv03 <vitosv03@gmail.com>
2021-11-20 18:44:55 +0200

    1. create blog model 2. registered log app in our site into settings.py 3. registered blog model for /admin

--- myawersomeblog-project/blog/models.py
+++ myawersomeblog-project/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+# 1. Create Post model. It should have the following attributes:
+# 	title
+# 	date
+# 	text
+# 	image
+#
+# 2. Make all needed operations to add Blog app with Post model to admin
+
+
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=300)
+    date = models.DateTimeField()
+    text = models.TextField()
+    image = models.ImageField(upload_to='blog_images/')
